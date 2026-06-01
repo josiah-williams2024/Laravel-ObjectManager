dd40da38de1701db04d824be3319ab2cac296fcb
josiah-williams2024 <josiah.williams.dev@gmail.com>
2026-06-01 09:16:27 -0400

    first commit

---
+++ app/Models/Note.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    protected $fillable = ['body', 'user_id'];
+
+    protected $casts = [
+        'body' => 'string',
+    ];
+
+
+}
---
+++ resources/js/pages/Dashboard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { Head, Link } from '@inertiajs/vue3';
+import { dashboard } from '@/routes';
+
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <header class="flex-end flex items-center justify-between p-3">
+        <div class="shawdow-lx rounded-full bg-gray-300 text-xl">
+            <Link class="">Create Notes</Link>
+        </div>
+        <div class="shawdow-lg rounded-full bg-gray-300 text-xl">
+            <Link>Update Note</Link>
+        </div>
+    </header>
+
+    <main class="space-around flex gap-3 p-4">
+
+    </main>
+</template>
