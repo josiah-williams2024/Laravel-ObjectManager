6a33561acd383d3efa1dafd5b5b8b819276b184c
josiah-williams2024 <josiah.williams.dev@gmail.com>
2026-06-01 09:29:54 -0400

    Create Routes for index,create and post notes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'Welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
+    Route::get('dashboar/notes', [NoteController::class, 'index'])->name('notes.index');
+    Route::get('dashboar/notes/create', [NoteController::class, 'create'])->name('notes.create');
+    Route::post('dashboar/notes', [NoteController::class, 'store'])->name('notes.store');
 });
 
 require __DIR__.'/settings.php';
