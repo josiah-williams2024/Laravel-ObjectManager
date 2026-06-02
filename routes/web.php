e8ee8b9ab60f3fdc11ef6d366e704cbbc37def97
josiah-williams2024 <josiah.williams.dev@gmail.com>
2026-06-02 09:04:15 -0400

    Add the routes to edit/delete

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboar/notes', [NoteController::class, 'index'])->name('notes.index');
     Route::get('dashboar/notes/create', [NoteController::class, 'create'])->name('notes.create');
     Route::post('dashboar/notes', [NoteController::class, 'store'])->name('notes.store');
+    Route::get('dashboard/{note}/edit', [NoteController::class, 'edit'])->name('notes.edit');
+    Route::put('dashboard/{note}', [NoteController::class, 'update'])->name('notes.update');
+    Route::delete('dashboard/{note}', [NoteController::class, 'destroy'])->name('notes.destroy');
 });
 
 require __DIR__.'/settings.php';
