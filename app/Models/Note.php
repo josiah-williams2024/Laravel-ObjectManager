3ea963d46224293863b38dc4b63858573926f818
josiah-williams2024 <josiah.williams.dev@gmail.com>
2026-06-02 16:47:24 -0400

    Create Model and Migration

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Note extends Model
 {
-    //
+    protected $fillable = ['title', 'content', 'user_id'];
+
+    protected $casts = [
+        'title' => 'string',
+        'content' => 'string',
+    ];
 }
