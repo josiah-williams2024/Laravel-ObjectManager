3b1ef4a39b790511bb841f8ac81684d69326e0cb
josiah-williams2024 <josiah.williams.dev@gmail.com>
2026-06-06 15:25:55 -0400

    Create the empty Pages for navigation later

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
 import { Head, Link } from '@inertiajs/vue3';
-import { dashboard } from '@/routes';
-
 </script>
 
 <template>
@@ -9,14 +7,15 @@ import { dashboard } from '@/routes';
 
     <header class="flex-end flex items-center justify-between p-3">
         <div class="shawdow-lx rounded-full bg-gray-300 text-xl">
-            <Link class="">Create Notes</Link>
+            <Link >View Notes</Link>
         </div>
-        <div class="shawdow-lg rounded-full bg-gray-300 text-xl">
-            <Link>Update Note</Link>
+        <div class="shawdow-lx rounded-full bg-gray-300 text-xl">
+            <Link>Create Notes</Link>
         </div>
+
     </header>
 
-    <main class="space-around flex gap-3 p-4">
-
+    <main class="space-around flex gap-3 p-4 justify-center">
+        Place Holder For Now
     </main>
 </template>
