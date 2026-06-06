28db91d5c09a0fa2f05850bc7aa43d0664c6080a
josiah-williams2024 <josiah.williams.dev@gmail.com>
2026-06-06 15:26:09 -0400

    Create the routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'Welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
+    Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
+    Route::get('/note/edit', [NoteController::class, 'edit'])->name('note.edit');
 });
 
 require __DIR__.'/settings.php';
