0adc7d73ffb2216932cbcad9c4819f8615e24963
josiah-williams2024 <josiah.williams.dev@gmail.com>
2026-06-09 09:41:00 -0400

    Create Car Model

---
+++ database/migrations/2026_06_09_133739_create_cars_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Car;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Car::class)->constrained()->cascadeOnDelete();
+            $table->string('brand');
+            $table->string('model');
+            $table->string('mileage');
+            $table->float('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
