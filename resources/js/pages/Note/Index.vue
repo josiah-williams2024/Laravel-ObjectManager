41f62aba039af435ac337803b321668d9148ea59
josiah-williams2024 <josiah.williams.dev@gmail.com>
2026-06-10 17:04:21 -0400

    Index page style and navigation

--- resources/js/pages/Note/Index.vue
+++ resources/js/pages/Note/Index.vue
@@ -1,7 +1,55 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3';
+import * as NoteController from '@/actions/App/Http/Controllers/NoteController';
+
+defineProps({
+    notes: Array,
+});
+</script>
 
 <template>
-    You have arrived at index
-</template>
+    <main class="min-h-screen p-4 md:p-6 lg:p-8">
+        <header class="mb-8">
+            <h1 class="text-center text-3xl font-bold">Notes</h1>
+        </header>
 
-<style scoped></style>
+        <section class="mx-auto mb-8 max-w-3xl">
+            <Link
+                class="inline-flex items-center rounded-full bg-blue-500 px-6 py-3 font-medium text-white transition hover:bg-blue-400 hover:shadow-2xl"
+                :href="NoteController.create()"
+            >
+                Create Note
+            </Link>
+        </section>
+
+        <section class="mx-auto grid max-w-6xl gap-4 md:grid-cols-2 lg:grid-cols-3">
+            <div v-if="notes?.length > 0">
+                <article
+                    v-for="note in notes"
+                    :key="note.id"
+                    class="rounded-lg border border-gray-200 bg-white p-5 shadow-sm hover:shadow-md"
+                >
+                        <Link
+                            class="block p-5"
+                            :href="NoteController.show(note.id)"
+                        >
+                    <div class="flex items-center justify-between">
+                        <div>
+                            <h2 class="text-lg font-semibold">
+                                {{ note.title }}
+                            </h2>
+
+                            <p class="mt-2 line-clamp-3 text-gray-600">
+                                {{ note.body }}
+                            </p>
+                        </div>
+
+                    </div>
+                        </Link>
+                </article>
+            </div>
+
+            <div v-else>There are no notes to view</div>
+        </section>
+    </main>
+</template>
