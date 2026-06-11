8e36fc6f52d9d5bbc45ef55d9446408e5e537c97
josiah-williams2024 <josiah.williams.dev@gmail.com>
2026-06-11 16:35:31 -0400

    basic edit logic done

--- resources/js/pages/Note/ViewNote.vue
+++ resources/js/pages/Note/ViewNote.vue
@@ -1,7 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3';
+import * as NoteController from '@/actions/App/Http/Controllers/NoteController';
+import type { Note } from '@/types/notes.ts';
+
+const props = defineProps<{
+    note: Note,
+}>();
+
+
+console.log(props.note);
+console.log(props.note.id);
+</script>
 
 <template>
     <p>You've arrived at the View note page</p>
+    {{props.note}}
+    <Link :href="NoteController.edit(props.note.id)"> Edit </Link>
 </template>
 
 <style scoped></style>
