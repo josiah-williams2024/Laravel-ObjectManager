d72cb47131b1fb7c3044fd7a0e738a753bd35dc3
josiah-williams2024 <josiah.williams.dev@gmail.com>
2026-06-11 16:35:38 -0400

    fix route

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,11 @@ Route::inertia('/', 'Welcome')->name('home');
 // Note Controller group
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
-    Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
-    Route::get('note/{edit}/edit', [NoteController::class, 'edit'])->name('note.edit');
     Route::get('note', [NoteController::class, 'index'])->name('note.index');
-    Route::get('note/{note}', [NoteController::class, 'show'])->name('note.show');
+    Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
     Route::post('note', [NoteController::class, 'store'])->name('note.store');
+    Route::get('note/{note}', [NoteController::class, 'show'])->name('note.show');
+    Route::get('note/{note}/edit', [NoteController::class, 'edit'])->name('note.edit');
     Route::patch('note/{note}', [NoteController::class, 'update'])->name('note.update');
     Route::delete('/note/{note}', [NoteController::class, 'destroy'])->name('note.destroy');
 });
